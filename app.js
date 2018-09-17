4e730c5b4b5644ffb298df78e7a5a05011fd4080
G-Chilie <okonkwolinda7@gmail.com>
2018-09-17 15:44:37 -0700

    Event Listener for delete

--- app.js
+++ app.js
@@ -45,6 +45,13 @@ UI.prototype.showAlert = function(message, className){
     }, 3000);
 }
 
+// Delete Book
+UI.prototype.deleteBook(target); {
+    if(target.className === 'delete') {
+        target.parentElement.parentElement.remove();
+    }
+}
+
 // Clear Fields
 UI.prototype.clearFields = function(){
     document.getElementById('title').value = '';
@@ -86,3 +93,14 @@ document.getElementById('book-form').addEventListener('submit',
         
   e.preventDefault();  
 });
+
+// Event Listener for delete
+document.getElementById('book-list').addEventListener
+('click', function(e){
+
+    console.log(123);
+    e.preventDefault();
+
+});
+
+}
