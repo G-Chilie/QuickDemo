4156f76cc9d27a099b8fd7f5b3c7554721cd2ae0
G-Chilie <okonkwolinda7@gmail.com>
2018-09-17 23:53:08 -0700

    Added book to list and show alert.

---
+++ appes6.js
@@ -0,0 +1,55 @@
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+class UI {
+    addBookToList(book) {
+    const list = document.getElementById('book-list'); 
+    // Create tr element
+    const row = document.createElement('tr');
+    // Insert cols
+    row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="delete">X<a></td>   
+         `;
+
+    list.appendChild(row);
+
+    }
+
+    showAlert() {
+        UI.prototype.showAlert = function(message, className){
+            // Create div
+            const div = document.createElement('div');
+            // Add classes
+            div.className = `alert ${className}`;
+            // Add text
+            div.appendChild(document.createTextNode(message));
+            // Get parent
+            const container = document.querySelector('.container');
+            // Get form  
+            const form = document.querySelector('#book-form');
+            // Insert alert
+            container.insertBefore(div, form);
+        
+            // Timeout after 3 sec
+            setTimeout(function(){
+                document.querySelector('.alert').remove();
+            }, 3000);
+
+    }
+
+    deleteBook() {
+
+    }
+
+    clearFields() {
+
+    }
+}
